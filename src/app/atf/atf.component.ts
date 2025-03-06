8c172c0478e3141de7aae29eab72de0bfac6e4ff
bethmannRolf <devak.robe@gmail.com>
2025-03-06 11:25:38 +0100

    link decoration footer

--- src/app/atf/atf.component.ts
+++ src/app/atf/atf.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-atf',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './atf.component.html',
   styleUrl: './atf.component.scss'
 })
