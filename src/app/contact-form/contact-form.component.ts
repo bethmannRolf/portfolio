fa7b4e0175aba490033e4f3778b81bf4a0fefe64
bethmannRolf <devak.robe@gmail.com>
2025-03-17 12:17:17 +0100

    debugged contact form comp

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  standalone: true,
-  imports: [],
+  standalone: true, 
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrls: ['./contact-form.component.css'],
+  imports: [ReactiveFormsModule] // 
 })
 export class ContactFormComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form data:', this.contactForm.value);
+    }
+  }
 }
