fc1f3893c44adf0323267ffc2ea3297990a5e145
bethmannRolf <devak.robe@gmail.com>
2025-03-20 13:12:04 +0100

    working on single quotations card structure

--- src/app/quotations/quotation-card/quotation-card.component.ts
+++ src/app/quotations/quotation-card/quotation-card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quotation-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quotation-card.component.html',
   styleUrl: './quotation-card.component.scss'
 })
--- src/app/quotations/quotations.component.ts
+++ src/app/quotations/quotations.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { QuotationCardComponent } from './quotation-card/quotation-card.component';
 
 @Component({
   selector: 'app-quotations',
   standalone: true,
-  imports: [],
+  imports: [QuotationCardComponent],
   templateUrl: './quotations.component.html',
   styleUrl: './quotations.component.scss'
 })
