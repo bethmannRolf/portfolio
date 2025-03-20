51ffb63c6c44ff42cca43739b8ec08f422615021
bethmannRolf <devak.robe@gmail.com>
2025-03-20 12:31:22 +0100

    added single quotation card

---
+++ src/app/quotations/quotation-card/quotation-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quotation-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './quotation-card.component.html',
+  styleUrl: './quotation-card.component.scss'
+})
+export class QuotationCardComponent {
+
+}
