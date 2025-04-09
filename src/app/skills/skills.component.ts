11b5c738727a0cff85c8c92ebcd7a8f5d753b072
bethmannRolf <devak.robe@gmail.com>
2025-04-09 08:18:01 +0200

    added skill img srcs

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+
+
+images: string[] = [
+  '../../assets/img/technologies/html.svg',
+  '../../assets/img/technologies/css.svg',
+  '../../assets/img/technologies/javascript.svg',
+  '../../assets/img/technologies/material_design.svg',
+  '../../assets/img/technologies/typescript.svg',
+  '../../assets/img/technologies/angular.svg',
+  '../../assets/img/technologies/firebase.svg',
+  '../../assets/img/technologies/git.svg',
+  '../../assets/img/technologies/rest-api.svg',
+  '../../assets/img/technologies/scrum.svg'
+
+
+
+]
+
+
 
 }
