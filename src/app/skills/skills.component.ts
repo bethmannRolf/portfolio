bcdd14b59a2e5be0ab8c2b9088cfd4e938a74ea3
bethmannRolf <devak.robe@gmail.com>
2025-04-09 08:56:36 +0200

    added object for skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -11,21 +11,32 @@ import { Component } from '@angular/core';
 export class SkillsComponent {
 
 
-images: string[] = [
-  '../../assets/img/technologies/html.svg',
-  '../../assets/img/technologies/css.svg',
-  '../../assets/img/technologies/javascript.svg',
-  '../../assets/img/technologies/material_design.svg',
-  '../../assets/img/technologies/typescript.svg',
-  '../../assets/img/technologies/angular.svg',
-  '../../assets/img/technologies/firebase.svg',
-  '../../assets/img/technologies/git.svg',
-  '../../assets/img/technologies/rest-api.svg',
-  '../../assets/img/technologies/scrum.svg'
+// images: string[] = [
+//   '../../assets/img/technologies/html.svg',
+//   '../../assets/img/technologies/css.svg',
+//   '../../assets/img/technologies/javascript.svg',
+//   '../../assets/img/technologies/material_design.svg',
+//   '../../assets/img/technologies/typescript.svg',
+//   '../../assets/img/technologies/angular.svg',
+//   '../../assets/img/technologies/firebase.svg',
+//   '../../assets/img/technologies/git.svg',
+//   '../../assets/img/technologies/rest-api.svg',
+//   '../../assets/img/technologies/scrum.svg'
+// ]
 
-
-
-]
+images = [
+{ src: '../../assets/img/technologies/html.svg', title: 'HTML' },
+{ src: '../../assets/img/technologies/css.svg', title: 'CSS' },
+{ src: '../../assets/img/technologies/javascript.svg', title: 'JavaScript' },
+{ src: '../../assets/img/technologies/material_design.svg', title: 'Material Design' },
+{ src: '../../assets/img/technologies/typescript.svg', title: 'TypeScript' },
+{ src: '../../assets/img/technologies/angular.svg', title: 'Angular' },
+{ src: '../../assets/img/technologies/firebase.svg', title: 'Firebase' },
+{ src: '../../assets/img/technologies/git.svg', title: 'GIT' },
+{ src: '../../assets/img/technologies/rest-api.svg', title: 'Res-Api' },
+{ src: '../../assets/img/technologies/scrum.svg', title: 'Scrum' },
+{ src: '../../assets/img/technologies/growth_mindset.svg', title: 'Growth Mindset' }
+];
 
 
 
