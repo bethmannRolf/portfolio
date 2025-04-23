7e734498da1756e1ea9fbb99967e29671d3cd363
bethmannRolf <devak.robe@gmail.com>
2025-04-23 07:55:17 +0200

    added for single projects first structure

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,34 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SingleProjectComponent } from './single-project/single-project.component';
 import { ProjectData } from '../models/project-data.model';
-
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [SingleProjectComponent],
+  imports: [CommonModule, SingleProjectComponent],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss']
+
 })
 export class ProjectsComponent {
-
-
- projectsData: ProjectData[] = [
-    { projectTitle: 'El Polo Loco' , imageSource: '../../assets/img/projects/epl_preview.png', usedTechnologies: 'JavaScript'+ 'HTML' + 'CSS', additionalInformation:'Jump, run and throw game based on object-oriented approach. Help PEpe to find coins and tabasco salsa to fight against the crazy hen.' },
-    { projectTitle: 'Join', imageSource: '../../assets/img/projects/join_preview.png' , usedTechnologies: 'JavaScript' +'Objektorientierung' + 'HTML' + 'CSS', additionalInformation:'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.'  },
-    { projectTitle: 'Portfolio' , imageSource: '../../assets/img/projects/portfolio_preview.png', usedTechnologies: 'Angular' + 'TypeScript' + 'HTML' + 'SCSS', additionalInformation:''  }
-  
-    ];
-    
-showProjectPreview(){
-
-}
-
-showSingleProject(){
-
-
-
-}
-
-
+  projectsData: ProjectData[] = [
+    {
+      projectTitle: 'El Polo Loco',
+      imageSource: 'assets/img/projects/epl_preview.png',
+      usedTechnologies: 'JavaScript HTML CSS',
+      additionalInformation: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.'
+    },
+    {
+      projectTitle: 'Join',
+      imageSource: 'assets/img/projects/join_preview.png',
+      usedTechnologies: 'JavaScript HTML CSS',
+      additionalInformation: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. '
+    }
+  ];
 }
