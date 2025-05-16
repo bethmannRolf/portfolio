e12f85be7aa368073f820fc8279d4cbb41827e78
bethmannRolf <devak.robe@gmail.com>
2025-05-16 13:40:35 +0200

    integrated font sources

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,16 @@
 
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
-  //  { path: '', component: MainPageComponent },
-  //   { path: '', component: MainPageComponent },
+   { path: '', component: ImprintComponent },
+    // { path: '', component: PrivacyPolicyComponent },
+    //  { path: '', component: LegalNoticeComponent },
  
 ];
