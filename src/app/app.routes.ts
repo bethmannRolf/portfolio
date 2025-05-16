4041bc94bfa18d259c9a4b8ac9f170aec0dd4175
bethmannRolf <devak.robe@gmail.com>
2025-05-16 12:35:22 +0200

    generated legal files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,5 +11,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
+  //  { path: '', component: MainPageComponent },
+  //   { path: '', component: MainPageComponent },
  
 ];
