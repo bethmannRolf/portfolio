0063a10d9bc5c42b4f1b7910626e22d2b3bd2208
bethmannRolf <devak.robe@gmail.com>
2025-05-27 21:52:25 +0200

    implemented coursor highlighting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
-
-
-
-// import { CursorHighlightComponent } from './cursor-highlight/cursor-highlight.component';
-// import { MainPageComponent } from './main-page/main-page.component';
-// import { ImprintComponent } from './imprint/imprint.component';
-// import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-// import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
-// import { FooterComponent } from './shared/footer/footer.component';
-
+import { CursorHighlightComponent } from './cursor-highlight/cursor-highlight.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, FooterComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, CursorHighlightComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
