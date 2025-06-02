8d47e9ea1bd6fbc8645e95103f97eae058b448b2
bethmannRolf <devak.robe@gmail.com>
2025-06-02 11:46:19 +0200

    added  ngx translations for imprint pp and ln

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
