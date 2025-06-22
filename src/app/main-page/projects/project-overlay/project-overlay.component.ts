8a3142fced7d766120ebfa4a3af6dc61e82adf0f
bethmannRolf <devak.robe@gmail.com>
2025-06-22 14:40:58 +0200

    deleted white spaces overlay scss

--- src/app/main-page/projects/project-overlay/project-overlay.component.ts
+++ src/app/main-page/projects/project-overlay/project-overlay.component.ts
@@ -5,32 +5,28 @@ import { ProjectData } from '../../../../app/models/project-data.model';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-project-overlay',
-    imports: [CommonModule, TranslateModule],
-    templateUrl: './project-overlay.component.html',
-    styleUrl: './project-overlay.component.scss'
+  selector: 'app-project-overlay',
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './project-overlay.component.html',
+  styleUrl: './project-overlay.component.scss'
 })
 export class ProjectOverlayComponent {
   @Input() project!: ProjectData;
-    @Input() allProjects: ProjectData[] = [];
-     @Output() changeProject = new EventEmitter<ProjectData>();
+  @Input() allProjects: ProjectData[] = [];
+  @Output() changeProject = new EventEmitter<ProjectData>();
   @Output() close = new EventEmitter<void>();
 
   onCloseClick() {
     this.close.emit();
   }
 
-    onBackdropClick(event: MouseEvent): void {
-    this.close.emit(); 
+  onBackdropClick(event: MouseEvent): void {
+    this.close.emit();
   }
 
-
- 
-
-nextProject() {
+  nextProject() {
     const currentIndex = this.allProjects.findIndex(p => p.projectNumber === this.project.projectNumber);
     const nextIndex = (currentIndex + 1) % this.allProjects.length;
     this.changeProject.emit(this.allProjects[nextIndex]);
   }
-
 }
